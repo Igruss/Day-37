8c9d7893e34448731b740399e48c3ccd12858d13
Igruss <ruller0611@gmail.com>
2020-08-09 16:03:12 -0500

    Capital one.java

--- src/day39_CustomClass/Employee.java
+++ src/day39_CustomClass/Employee.java
@@ -9,11 +9,11 @@ public class Employee {
 
     String name;
     char gender;
-    long SSN;
+    String SSN;
     String jobTitle;
     double salary;
 
-    public void setEmployeeInfo(String employeeName, char employeeGender, long employeeSSN, String employeeJobTitle, double employeeSalary){
+    public void setEmployeeInfo(String employeeName, char employeeGender, String employeeSSN, String employeeJobTitle, double employeeSalary){
         name = employeeName;
         gender = employeeGender;
         SSN = employeeSSN;
