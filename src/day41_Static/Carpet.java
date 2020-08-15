1056c5f28dd5a41e5b558e6e247857985317e5dd
Igruss <ruller0611@gmail.com>
2020-08-15 12:02:34 -0500

    Day 41 Class

--- src/day41_Static/Carpet.java
+++ src/day41_Static/Carpet.java
@@ -19,31 +19,35 @@ public class Carpet {
     double unitPrice;
     boolean isPersian;
 
-    public void customOrder(double width,double length,double unitPrice,boolean isPersian){
-        this.width =width;
+    public void customOrder(double width, double length, double unitPrice, boolean isPersian) {
+        this.width = width;
         this.length = length;
-        this.unitPrice= unitPrice;
+        this.unitPrice = unitPrice;
         this.isPersian = isPersian;
 
 
-
-
     }
 
-    public double calCost(){
-        double totalPrice =(width*length)* unitPrice;
-        return (isPersian)? totalPrice +200 : totalPrice;
+    public double calCost() {
+        double totalPrice = (width * length) * unitPrice;
+        return (isPersian) ? totalPrice + 200 : totalPrice;
     }
 
-    public void getCarpetInfo (){
+    public void getCarpetInfo() {
         System.out.println("==================================");
-        System.out.println("Width: "+width);
-        System.out.println("Length: "+length);
-        System.out.println("Unit Price: "+unitPrice);
-        System.out.println("Persian Carpet: "+isPersian);
-        System.out.println("Totoal Price: "+calCost());
+        System.out.println("Width: " + width);
+        System.out.println("Length: " + length);
+        System.out.println("Unit Price: " + unitPrice);
+        System.out.println("Persian Carpet: " + isPersian);
+        System.out.println("Totoal Price: " + calCost());
         System.out.println("==================================");
+
+
     }
 
+    public String toString(){
+        return "Width: "+width + ", Length: "+length + ", Persian: "+isPersian;
+
+    }
 
 }
---
+++ src/day41_Static/carObjects.java
@@ -0,0 +1,14 @@
+package day41_Static;
+
+public class carObjects {
+
+    public static void main(String[] args) {
+
+        Car c1 = new Car();
+        c1.setCarInfo("BMW","X5",2019,50000,"Black");
+
+        System.out.println(c1);
+
+    }
+
+}
