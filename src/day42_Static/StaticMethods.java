c1ac11fb7d07606486989e26371f8fe2ba3bb6d2
Igruss <ruller0611@gmail.com>
2020-08-16 15:02:07 -0500

    Day 42 Class

---
+++ src/day42_Static/StaticMethods.java
@@ -0,0 +1,17 @@
+package day42_Static;
+
+public class StaticMethods {
+
+    int a = 200;
+    static int b = 400;
+
+    public static void main(String[] args) {
+
+        System.out.println(b);
+       // System.out.println(a); Cannot work (only way is to call it )
+
+        StaticMethods obj = new StaticMethods();
+        System.out.println(obj.a);
+
+    }
+}
