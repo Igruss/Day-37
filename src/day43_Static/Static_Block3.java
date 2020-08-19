16d9f8165a97f59c5f8be2fc5542f5026b11ecb8
Igruss <ruller0611@gmail.com>
2020-08-18 20:41:48 -0500

    Day 43 Class

---
+++ src/day43_Static/Static_Block3.java
@@ -0,0 +1,27 @@
+package day43_Static;
+
+public class Static_Block3 {
+
+    static int a;
+    static int b;
+    static int c;
+
+/*
+    public static void main(String[] args) {
+
+        a = 100;
+        b = 200;
+        c = 300;
+
+
+
+    }
+*/
+
+    static {
+        a = 100;
+        b = 200;
+        c = 300;
+    }
+
+}
---
+++ src/day43_Static/Test.java
@@ -0,0 +1,18 @@
+package day43_Static;
+
+import java.util.Arrays;
+
+public class Test {
+
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println( Arrays.toString(Data.arr) );
+        System.out.println(Data.list);
+
+
+    }
+
+}
