86c1f9ad38c8ed22b40917f7c598fbce090361ae
Igruss <ruller0611@gmail.com>
2020-08-18 20:59:38 -0500

    Day 43 Class

--- src/Library/Timer.java
+++ src/Library/Timer.java
@@ -5,26 +5,23 @@ import java.util.*;
 
 public class Timer {
 
-    public static void main(String[] args)throws Exception {
+    public static void main(String[] args) throws Exception {
 
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Num of min:");
         int min = scan.nextInt();
-        for(int i = min; i>0 ; --i){
-            if(i == 0)
+        for (int i = min; i > 0; --i) {
+            if (i == 0)
                 break;
-            for(int z =59 ;z >=0 ;--z){
-                System.out.println((i-1)+" minutes and "+z+" seconds left");
+            for (int z = 59; z >= 0; --z) {
+                System.out.println((i - 1) + " minutes and " + z + " seconds left");
                 Thread.sleep(1000);
             }
         }
         System.out.println("Time is up");
 
 
-
-
-
-
     }
+
 }
--- src/day43_Static/Test.java
+++ src/day43_Static/Test.java
@@ -2,9 +2,9 @@ package day43_Static;
 
 import java.util.Arrays;
 
+import static Library.Resources.a;// only imports static variable
+
 public class Test {
-
-
 
 
     public static void main(String[] args) {
@@ -12,6 +12,9 @@ public class Test {
         System.out.println( Arrays.toString(Data.arr) );
         System.out.println(Data.list);
 
+        System.out.println("==============================================");
+
+        System.out.println(a);
 
     }
 
