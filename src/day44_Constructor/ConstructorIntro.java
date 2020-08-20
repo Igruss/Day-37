24c0d821f026ca82b5c8e325ba97bb2c50cabfbb
Igruss <ruller0611@gmail.com>
2020-08-19 21:07:16 -0500

    Day 44 Class

--- src/day44_Constructor/ConstructorIntro.java
+++ src/day44_Constructor/ConstructorIntro.java
@@ -4,11 +4,15 @@ public class ConstructorIntro {
 
     public ConstructorIntro(String arr){
         System.out.println("Hello "+arr);
+        System.out.println("How are you "+arr);
     }
     public static void main(String[] args) {
 
       //  ConstructorIntro obj = new ConstructorIntro(10);
           ConstructorIntro obj = new ConstructorIntro("Igor");
+          ConstructorIntro obj1 = new ConstructorIntro("Dawud");
+          ConstructorIntro obj2 = new ConstructorIntro("Zeliha");
+
 
 
     }
