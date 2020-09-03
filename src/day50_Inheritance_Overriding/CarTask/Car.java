50e237cf04b15f21020975a2754e008582e477fc
Igruss <ruller0611@gmail.com>
2020-09-02 19:47:04 -0500

    Day 50.java

---
+++ src/day50_Inheritance_Overriding/CarTask/Car.java
@@ -0,0 +1,19 @@
+package day50_Inheritance_Overriding.CarTask;
+/*
+
+Honda:
+    start(): Insert the key
+             twist ignition key to start
+
+ */
+public class Car {
+
+    public void start(){
+
+        System.out.println("Insert the key");
+        System.out.println("twist ignition key");
+
+    }
+
+
+}
