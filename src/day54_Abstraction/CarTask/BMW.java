20ee94892e0f273ec66855c0f238d38c5e8331d0
Igruss <ruller0611@gmail.com>
2020-09-15 20:56:46 -0500

    Day 54.java

---
+++ src/day54_Abstraction/CarTask/BMW.java
@@ -0,0 +1,10 @@
+package day54_Abstraction.CarTask;
+
+public  class BMW extends Car{
+
+
+    @Override
+    public void start() {
+        System.out.println("Starting BWM by: push start button");
+    }
+}
