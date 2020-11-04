0a04f89ec41f78a733c37e5428692e29af2900fc
Igruss <ruller0611@gmail.com>
2020-11-04 17:41:45 -0600

    Office Hours

---
+++ src/Office_Hours/Practice_11_04_2020/MapBully.java
@@ -0,0 +1,23 @@
+package Office_Hours.Practice_11_04_2020;
+
+import java.util.Map;
+
+public class MapBully {
+
+    public Map<String, String> mapBully(Map<String, String> map) {
+
+        if(map.containsKey("a")) {
+            String value = map.get("a");
+            map.put("b", value);
+            map.put("a", "");
+        }
+
+        return map;
+
+    }
+
+
+
+
+
+}
